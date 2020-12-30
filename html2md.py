7884bd736788abe1f22fef60391fe18966153355
c-yan <c-yan@users.noreply.github.com>
2020-12-31 02:15:41 +0900

    現行バージョンの取り込み

---
+++ html2md.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import fileinput
+import re
+import sys
+
+codepattern = re.compile('</?code>')
+apattern = re.compile('<a href="([^"]+)">(.+?)</a>')
+h2pattern = re.compile('<h2[^>]+>(.+?)</h2>')
+h3pattern = re.compile('<h3[^>]+>(.+?)</h3>')
+h4pattern = re.compile('<h4[^>]+>(.+?)</h4>')
+dlpattern = re.compile('<dl[^>]+><dt>(.+?)</dt>')
+
+for line in fileinput.input():
+    line = h2pattern.sub(r'# \1', line)
+    line = h3pattern.sub(r'## \1', line)
+    line = h4pattern.sub(r'### \1', line)
+    line = dlpattern.sub(r'### \1', line)
+    line = codepattern.sub(r'`', line)
+    line = apattern.sub(r'[\2](\1)', line)
+    line = line.replace('](/', '](https://golang.org/')
+    sys.stdout.write(line)
